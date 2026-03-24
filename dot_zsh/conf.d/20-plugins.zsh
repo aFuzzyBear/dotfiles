1f8ef6a3739104b5b5028f842ab5c64f8844353a
Fuzzy Bear <28299972+aFuzzyBear@users.noreply.github.com>
2026-03-24 19:07:38 +0000

    feat: add 20-plugins.zsh for fzf-tab and zsh enhancements
    Signed-off-by: Fuzzy Bear <28299972+aFuzzyBear@users.noreply.github.com>

---
+++ dot_zsh/conf.d/20-plugins.zsh
@@ -0,0 +1,6 @@
+# fzf-tab — must load after compinit, before other plugins
+source ~/.zsh/plugins/fzf-tab/fzf-tab.plugin.zsh
+
+# Autosuggestions + syntax highlighting
+source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
