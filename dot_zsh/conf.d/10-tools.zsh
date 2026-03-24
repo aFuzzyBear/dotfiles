f07d2247007c64d8e749021806e53eb4223dbb5e
Fuzzy Bear <28299972+aFuzzyBear@users.noreply.github.com>
2026-03-24 19:07:26 +0000

    feat: add 10-tools.zsh for shell tool initialization
    This commit initialises:
    Mise, Starship, fzf, zoxide & atuin within the zshell
    
    Signed-off-by: Fuzzy Bear <28299972+aFuzzyBear@users.noreply.github.com>

---
+++ dot_zsh/conf.d/10-tools.zsh
@@ -0,0 +1,14 @@
+# mise
+eval "$(~/.local/bin/mise activate zsh)"
+
+# starship
+eval "$(starship init zsh)"
+
+# fzf
+eval "$(fzf --zsh)"
+
+# zoxide
+eval "$(zoxide init zsh)"
+
+# atuin
+eval "$(atuin init zsh)"
